e905fd2703952f8a0d1c128790337755e1648a78
peithopipeline <peithopipeline@gmail.com>
2026-04-03 12:33:35 +1100

    Update home_assistant_api.rb

--- app/apis/home_assistant_api.rb
+++ app/apis/home_assistant_api.rb
@@ -76,7 +76,7 @@ class HomeAssistantApi
 
   def top_left
     data
-      .select { it[:entity_id].start_with?("sensor.timeframe_top_left") && it[:state].present? }
+      .select { it[:entity_id].start_with?("binary_sensor.framedleft") && it[:state].present? }
       .filter_map do
         parts = it[:state].split(",").map(&:strip)
         next if parts.length < 2
